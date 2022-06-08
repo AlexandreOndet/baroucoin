8759076d271e3062240ef73991689749db9722d7
Maxim-Durand <durandmaxime1931@gmail.com>
2022-06-07 23:50:57 -0400

    Refactored project structure to avoid client and server coupling. Added peers.json as our "DNS" list used by the server instead of a real database. Added README entries to explain how to use the Dockerfile and deploy the server.

--- server/main.py
+++ server/main.py
@@ -1,8 +1,10 @@
+from typing import Set, Dict, Any
+
 from fastapi import FastAPI, Request
+import json
 
 blockchain = FastAPI()
-
-chain = [] # Replace by our blockchain object later on
+PEERS_JSON_PATH = "/server/peers.json"
 
 
 @blockchain.get("/test")
@@ -25,32 +27,42 @@ async def b_chain() -> dict:
     Ledger endpoint for Blockchain server. Returns the current state of the blockchain.
     :return: dict -> keys: chain:Lis[dict] -> Serialized Blocks
     """
-    return {"current_chain": chain.blocks}
+    blocks = []  # TODO implement the necessary logic to fetch current state of blockchain
+    return {"current_chain": blocks}
 
 
 @blockchain.get("/peers")
-async def peers() -> dict:
+async def peers() -> dict[str, Any]:
     """
     Peers List endpoint for Blockchain Server.
     :return: dict -> keys: peers: List[str]
     """
-    ips = [peer.ip for peer in chain.peers]
-    return {"peers": ips}
+    with open(PEERS_JSON_PATH, "r+") as f:
+        peers = json.loads(f.read())
+        # ips = [peer.ip for peer in peers.keys()]
+        return {"peers": peers}
 
 
-@blockchain.post("/new-peer")
+@blockchain.get("/new-peer")
 async def new_peer(request: Request) -> dict:
     """
     Adds a new peer to the blockchain network
     :param request: Request -> HTTP POST request.
     :return: dict -> keys: new_peer:str -> IP Address
     """
+    server_address = "?whatsmyip?"
     peer_host = request.client.host
     peer_port = request.client.port
     if peer_host == "127.0.0.1":
-        address = f"{chain.server_address}"
+        address = f"{server_address}"
     else:
         address = f"http://{peer_host}:{peer_port}"
     print(address)
-    chain.peers.add(address)
-    return {"new_peer added ": address}
+    with open(PEERS_JSON_PATH, "r") as f:
+        data = json.loads(f.read())
+    if address in data:
+        return {"Following address is already registered as a node": address}
+    data[address] = {}
+    with open(PEERS_JSON_PATH, 'w') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+    return {"New peer registered with following address": address}
