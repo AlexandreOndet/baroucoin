8e3444439dcf73a66ac65df05054722d1e77e449
Alexandre Ondet <alexandre.ondet@telecomnancy.net>
2022-06-08 22:42:00 -0400

    fix logique des transactions

--- app/Transaction.py
+++ app/Transaction.py
@@ -1,15 +1,13 @@
 class Transaction:
     def __init__(self, senders, receivers):
-        tmp = 0
+        total_in = 0
         for i in senders:
-            tmp += i[1]
-        if tmp != 1.0:
-            raise ValueError("Sum of coefs for senders it not equal to one")
-        self.senders = []  # tuples with address (string) and coef double. Sum of coefs must be equal to 1.0
+            total_in += i[1]
+        self.senders = []  # tuples with address (string) and amount of coins.
 
-        tmp = 0
+        total_out = 0
         for i in receivers:
-            tmp += i[1]
-        if tmp != 1.0:
-            raise ValueError("Sum of coefs for receivers it not equal to one")
+            total_out += i[1]
+        if total_in >= total_out:
+            raise ValueError("Sum of amount in must be >= Sum of amount out")
         self.receivers = []
