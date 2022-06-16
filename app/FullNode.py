5e497556c35af2782c646d8cf47c31f71f00415b
Donneger Etienne <23462475+Krow10@users.noreply.github.com>
2022-06-15 20:33:09 -0400

    Adapted FullNode for new consensusAlgorithm usage

--- app/FullNode.py
+++ app/FullNode.py
@@ -5,14 +5,14 @@ from Blockchain import *
 from Wallet import *
 from Transaction import *
 from TransactionStore import *
-from ConsensusAlgorithm import *
+from ProofOfWork import *
 
 class FullNode(object):
     """docstring for FullNode"""
-    def __init__(self, consensusAlgorithm: ConsensusAlgorithm, existing_wallet: Wallet):
+    def __init__(self, consensusAlgorithm: bool, existing_wallet: Wallet):
         super(FullNode, self).__init__()
         self.wallet = existing_wallet
-        self.consensusAlgorithm = consensusAlgorithm
+        self.consensusAlgorithm = ProofOfWork(1) if not consensusAlgorithm else None # TODO : Change to ProofOfStake and set difficulty accordingly
         self.transaction_pool = []
         self.blockchain = Blockchain() # Should the FullNode class search for existing blockchain or should the constructor of Blockchain do it ?
 
