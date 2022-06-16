3bfdc61ff72ae62fd4b8899a866da494665eeb56
Alexandre Ondet <alexandre.ondet@telecomnancy.net>
2022-06-15 21:03:19 -0400

    premier test qui marche (nb de zeros)

--- app/Tester.py
+++ app/Tester.py
@@ -1,14 +1,16 @@
 import unittest
-
+from ProofOfWork import *
 from Blockchain import *
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
+    def test_amount_of_zeros(self):
         chain = Blockchain()
         chain.createGenesisBlock()
-        print(str(chain.lastBlock))
-        self.assertEqual(True, True)  # add assertion here
+        PoW = ProofOfWork(1)
+        print("Nonce="+str(PoW.mine(chain.lastBlock)))
+        print("Hash="+chain.lastBlock.getHash())
+        self.assertEqual(chain.lastBlock.getHash()[0:PoW.blockDifficulty], '0'*PoW.blockDifficulty)  # add assertion here
 
 
 if __name__ == '__main__':
