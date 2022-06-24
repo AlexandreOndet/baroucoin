c1db070485058f2ab4b2338642e30e23167be1b0
Donneger Etienne <23462475+Krow10@users.noreply.github.com>
2022-06-24 01:45:29 -0400

    Correction de la non assignation des paramètres constructeur

--- app/Transaction.py
+++ app/Transaction.py
@@ -3,14 +3,14 @@ class Transaction:
         total_in = 0
         for i in senders:
             total_in += i[1]
-        self.senders = []  # tuples with address (string) and amount of coins.
+        self.senders = senders  # tuples with address (string) and amount of coins.
 
         total_out = 0
         for i in receivers:
             total_out += i[1]
         if total_out > total_in:
             raise ValueError("Sum of amount in must be >= Sum of amount out")
-        self.receivers = []
+        self.receivers = receivers
 
     def __repr__(self):
         return f"in:{self.senders}, out:{self.receivers}"
