ec797d2b2b2b1e4e2d1c9b1a3b41c004238ab622
Alexandre Ondet <alexandre.ondet@telecomnancy.net>
2022-06-25 13:44:31 -0400

    un peu de refactoring, et ajout de tests sur les transactions

--- app/FullNode.py
+++ app/FullNode.py
@@ -7,14 +7,17 @@ from Transaction import *
 from TransactionStore import *
 from ProofOfWork import *
 
+
 class FullNode(object):
     """docstring for FullNode"""
+
     def __init__(self, consensusAlgorithm: bool, existing_wallet: Wallet):
         super(FullNode, self).__init__()
         self.wallet = existing_wallet
-        self.consensusAlgorithm = ProofOfWork(1) if not consensusAlgorithm else None # TODO : Change to ProofOfStake and set difficulty accordingly
+        self.consensusAlgorithm = ProofOfWork(
+            1) if not consensusAlgorithm else None  # TODO : Change to ProofOfStake and set difficulty accordingly
         self.transaction_pool = []
-        self.blockchain = Blockchain() # Should the FullNode class search for existing blockchain or should the constructor of Blockchain do it ?
+        self.blockchain = Blockchain()  # Should the FullNode class search for existing blockchain or should the constructor of Blockchain do it ?
 
     def addToTransactionPool(self, t: Transaction):
         self.transaction_pool.append(t)
@@ -22,16 +25,16 @@ class FullNode(object):
     def createNewBlock(self) -> Block:
         previous_block = self.blockchain.lastBlock
         return Block(
-            timestamp=time.time(), 
-            transactionStore=TransactionStore(self.transaction_pool), 
-            height=previous_block.height + 1, 
+            timestamp=time.time(),
+            transactionStore=TransactionStore(self.transaction_pool),
+            height=previous_block.height + 1,
             consensusAlgorithm=self.consensusAlgorithm,
             previousHash=previous_block.getHash(),
             miner=self.wallet.address,
             reward=self.computeReward())
 
     def computeReward(self) -> int:
-        return 1 # TODO : Compute reward, maybe according to consensus algorithm or external rules ?*
+        return 1  # TODO : Compute reward, maybe according to consensus algorithm or external rules ?*
 
     def mineNewBlock(self):
         self.consensusAlgorithm.mine(self.createNewBlock())
@@ -39,23 +42,37 @@ class FullNode(object):
     '''
         See https://github.com/bitcoinbook/bitcoinbook/blob/develop/ch10.asciidoc#independent-verification-of-transactions for reference
     '''
+
     def validateTransaction(self, t: Transaction) -> bool:
-        return (any(t.senders) 
-        and any(t.receivers) 
-        and len(t.senders) == len(set(t.senders)) # Check for duplicate inputs
-        )
+        if not (any(t.senders)
+                and any(t.receivers)
+                and len(t.senders) == len(set(t.senders))):
+            return False
+
+        for i in t.senders:
+            exists = False
+            for j in self.blockchain.blockChain:
+                for k in j.transactionStore.transactions:
+                    if i in k.receivers:
+                        exists = True
+            if not exists:
+                return False
+
+        return True  # Check for duplicate inputs
 
     '''
         See https://github.com/bitcoinbook/bitcoinbook/blob/develop/ch10.asciidoc#validating-a-new-block for reference
     '''
+
     def validateNewBlock(self, newBlock: Block) -> bool:
-        if (newBlock.getHash()[0:self.consensusAlgorithm.blockDifficulty] != '0' * self.consensusAlgorithm.blockDifficulty
-            or newBlock.timestamp - time.time() > 3600 # Prevent block from being too much in the future (1h max)
-            or newBlock.reward != self.computeReward()):
+        if (newBlock.getHash()[
+            0:self.consensusAlgorithm.blockDifficulty] != '0' * self.consensusAlgorithm.blockDifficulty
+                or newBlock.timestamp - time.time() > 3600  # Prevent block from being too much in the future (1h max)
+                or newBlock.reward != self.computeReward()):
             return False
 
-        return all([self.validateTransaction(t) for t in newBlock.transactionStore.transactions]) # Validate each transaction
-            
+        return all(
+            [self.validateTransaction(t) for t in newBlock.transactionStore.transactions])  # Validate each transaction
 
 # node = FullNode(None, Wallet("test"))
 # print(vars(node.createNewBlock()))
--- app/Tester.py
+++ app/Tester.py
@@ -6,74 +6,115 @@ from Blockchain import *
 from FullNode import *
 from Transaction import *
 
+
 class PoWTests(unittest.TestCase):
     def test_difficulty_amount_of_zeros(self):
         chain = Blockchain()
         chain.createGenesisBlock()
         PoW = ProofOfWork(1)
         PoW.mine(chain.lastBlock)
-        self.assertEqual(chain.lastBlock.getHash()[0:PoW.blockDifficulty], '0'*PoW.blockDifficulty, f"PoW difficulty broken : hash={chain.lastBlock.getHash()}, difficulty={PoW.blockDifficulty}")
+        self.assertEqual(chain.lastBlock.getHash()[0:PoW.blockDifficulty], '0' * PoW.blockDifficulty,
+                         f"PoW difficulty broken : hash={chain.lastBlock.getHash()}, difficulty={PoW.blockDifficulty}")
 
     def test_block_validation_difficulty(self):
         node = FullNode(consensusAlgorithm=False, existing_wallet=Wallet(""))
-        block = Block(timestamp=time.time(), transactionStore=TransactionStore(), height=0, consensusAlgorithm=node.consensusAlgorithm, previousHash=0, miner=0, reward=node.computeReward(), nonce=0)
-        
-        while block.getHash()[0] == '0': # Generate a block with hash not validating any Proof of Work (no zeroes at start)
-            block = Block(timestamp=time.time(), transactionStore=TransactionStore(), height=0, consensusAlgorithm=node.consensusAlgorithm, previousHash=0, miner=0, reward=node.computeReward(), nonce=0)
-        self.assertFalse(node.validateNewBlock(block), f"Non-mined block passes as valid block : hash={block.getHash()}, difficulty={node.consensusAlgorithm.blockDifficulty}")
-        
-        node.consensusAlgorithm.mine(block) # Genuine mined block will pass the test
-        self.assertTrue(node.validateNewBlock(block), f"Mined block is not validated : hash={block.getHash()}, difficulty={node.consensusAlgorithm.blockDifficulty}")
+        block = Block(timestamp=time.time(), transactionStore=TransactionStore(), height=0,
+                      consensusAlgorithm=node.consensusAlgorithm, previousHash=0, miner=0, reward=node.computeReward(),
+                      nonce=0)
+
+        while block.getHash()[
+            0] == '0':  # Generate a block with hash not validating any Proof of Work (no zeroes at start)
+            block = Block(timestamp=time.time(), transactionStore=TransactionStore(), height=0,
+                          consensusAlgorithm=node.consensusAlgorithm, previousHash=0, miner=0,
+                          reward=node.computeReward(), nonce=0)
+        self.assertFalse(node.validateNewBlock(block),
+                         f"Non-mined block passes as valid block : hash={block.getHash()}, difficulty={node.consensusAlgorithm.blockDifficulty}")
+
+        node.consensusAlgorithm.mine(block)  # Genuine mined block will pass the test
+        self.assertTrue(node.validateNewBlock(block),
+                        f"Mined block is not validated : hash={block.getHash()}, difficulty={node.consensusAlgorithm.blockDifficulty}")
 
     def test_block_validation_timestamp(self):
         node = FullNode(consensusAlgorithm=False, existing_wallet=Wallet(""))
-        
+
         # Timestamp 1 day in the future
-        invalid_block = Block(timestamp=time.time() + 24*3600, transactionStore=TransactionStore(), height=0, consensusAlgorithm=node.consensusAlgorithm, previousHash=0, miner=0, reward=node.computeReward(), nonce=0)
-        node.consensusAlgorithm.mine(invalid_block) # Don't forget to mine the block for valid hash
-        self.assertFalse(node.validateNewBlock(invalid_block), f"Invalid timestamp for block passes as valid block : timestamp={invalid_block.timestamp}, diff={invalid_block.timestamp - time.time()}")
-        
+        invalid_block = Block(timestamp=time.time() + 24 * 3600, transactionStore=TransactionStore(), height=0,
+                              consensusAlgorithm=node.consensusAlgorithm, previousHash=0, miner=0,
+                              reward=node.computeReward(), nonce=0)
+        node.consensusAlgorithm.mine(invalid_block)  # Don't forget to mine the block for valid hash
+        self.assertFalse(node.validateNewBlock(invalid_block),
+                         f"Invalid timestamp for block passes as valid block : timestamp={invalid_block.timestamp}, diff={invalid_block.timestamp - time.time()}")
+
         # Correct timestamp
-        valid_block = Block(timestamp=time.time(), transactionStore=TransactionStore(), height=0, consensusAlgorithm=node.consensusAlgorithm, previousHash=0, miner=0, reward=node.computeReward(), nonce=0)
-        node.consensusAlgorithm.mine(valid_block) # Don't forget to mine the block for valid hash
-        self.assertTrue(node.validateNewBlock(valid_block), f"Correct timestamp for block is not validated : timestamp={valid_block.timestamp}, diff={valid_block.timestamp - time.time()}")
+        valid_block = Block(timestamp=time.time(), transactionStore=TransactionStore(), height=0,
+                            consensusAlgorithm=node.consensusAlgorithm, previousHash=0, miner=0,
+                            reward=node.computeReward(), nonce=0)
+        node.consensusAlgorithm.mine(valid_block)  # Don't forget to mine the block for valid hash
+        self.assertTrue(node.validateNewBlock(valid_block),
+                        f"Correct timestamp for block is not validated : timestamp={valid_block.timestamp}, diff={valid_block.timestamp - time.time()}")
 
     def test_block_validation_reward(self):
         node = FullNode(consensusAlgorithm=False, existing_wallet=Wallet(""))
-        
+
         # Invalid null reward
-        invalid_block = Block(timestamp=time.time(), transactionStore=TransactionStore(), height=0, consensusAlgorithm=node.consensusAlgorithm, previousHash=0, miner=0, reward=0, nonce=0)
-        node.consensusAlgorithm.mine(invalid_block) # Don't forget to mine the block for valid hash
-        self.assertFalse(node.validateNewBlock(invalid_block), f"Invalid reward for block passes as valid block : reward={invalid_block.reward}, computed={node.computeReward()}")
-        
+        invalid_block = Block(timestamp=time.time(), transactionStore=TransactionStore(), height=0,
+                              consensusAlgorithm=node.consensusAlgorithm, previousHash=0, miner=0, reward=0, nonce=0)
+        node.consensusAlgorithm.mine(invalid_block)  # Don't forget to mine the block for valid hash
+        self.assertFalse(node.validateNewBlock(invalid_block),
+                         f"Invalid reward for block passes as valid block : reward={invalid_block.reward}, computed={node.computeReward()}")
+
         # Correct reward
-        valid_block = Block(timestamp=time.time(), transactionStore=TransactionStore(), height=0, consensusAlgorithm=node.consensusAlgorithm, previousHash=0, miner=0, reward=node.computeReward(), nonce=0)
-        node.consensusAlgorithm.mine(valid_block) # Don't forget to mine the block for valid hash
-        self.assertTrue(node.validateNewBlock(valid_block), f"Correct reward for block is not validated : reward={valid_block.reward}, computed={node.computeReward()}")
+        valid_block = Block(timestamp=time.time(), transactionStore=TransactionStore(), height=0,
+                            consensusAlgorithm=node.consensusAlgorithm, previousHash=0, miner=0,
+                            reward=node.computeReward(), nonce=0)
+        node.consensusAlgorithm.mine(valid_block)  # Don't forget to mine the block for valid hash
+        self.assertTrue(node.validateNewBlock(valid_block),
+                        f"Correct reward for block is not validated : reward={valid_block.reward}, computed={node.computeReward()}")
 
     def test_block_validation_transactions(self):
         node = FullNode(consensusAlgorithm=False, existing_wallet=Wallet(""))
-        block = Block(timestamp=time.time(), transactionStore=TransactionStore(), height=0, consensusAlgorithm=node.consensusAlgorithm, previousHash=0, miner=0, reward=node.computeReward(), nonce=0)
-        
+        block = Block(timestamp=time.time(), transactionStore=TransactionStore(), height=0,
+                      consensusAlgorithm=node.consensusAlgorithm, previousHash=0, miner=0, reward=node.computeReward(),
+                      nonce=0)
+        t = Transaction(senders=[(Wallet("first").address, 1)], receivers=[(Wallet("first").address, 1)])
+        b = Block(timestamp=time.time(), transactionStore=TransactionStore(), height=0,
+                  consensusAlgorithm=node.consensusAlgorithm, previousHash=0, miner=0, reward=node.computeReward(),
+                  nonce=0)
+        b.transactionStore.addTransaction(t)
+        node.blockchain.blockChain.append(b)  # set up a transaction to have a valid output later
         # Generate valid transaction
-        valid_transaction = Transaction(senders=[(Wallet("first").address, 1)], receivers=[(Wallet("second").address, 1)])
+        valid_transaction = Transaction(senders=[(Wallet("first").address, 1)],
+                                        receivers=[(Wallet("second").address, 1)])
         block.transactionStore.addTransaction(valid_transaction)
-        node.consensusAlgorithm.mine(block) # Don't forget to mine the block for valid hash
-        self.assertTrue(node.validateNewBlock(block), f"Correct transactions for block are not validated : transactions={block.transactionStore.transactions}")
-        
+        node.consensusAlgorithm.mine(block)  # Don't forget to mine the block for valid hash
+        self.assertTrue(node.validateNewBlock(block),
+                        f"Correct transactions for block are not validated : transactions={block.transactionStore.transactions}")
+
         # Generate invalid empty transaction
         invalid_transaction = Transaction(senders=[], receivers=[])
         block.transactionStore.addTransaction(invalid_transaction)
-        node.consensusAlgorithm.mine(block) # Don't forget to mine the block for valid hash
-        self.assertFalse(node.validateNewBlock(block), f"Invalid transaction for block gets validated : transactions={block.transactionStore.transactions}")
+        node.consensusAlgorithm.mine(block)  # Don't forget to mine the block for valid hash
+        self.assertFalse(node.validateNewBlock(block),
+                         f"Invalid transaction for block gets validated : transactions={block.transactionStore.transactions}")
 
     def test_transaction_validation(self):
         node = FullNode(consensusAlgorithm=False, existing_wallet=Wallet(""))
+        t = Transaction(senders=[(Wallet("first").address, 1)], receivers=[(Wallet("first").address, 1)])
+        b = Block(timestamp=time.time(), transactionStore=TransactionStore(), height=0,
+                  consensusAlgorithm=node.consensusAlgorithm, previousHash=0, miner=0, reward=node.computeReward(),
+                  nonce=0)
+        b.transactionStore.addTransaction(t)
+        node.blockchain.blockChain.append(b)  # set up a transaction to have a valid output later
         self.assertFalse(node.validateTransaction(Transaction(senders=[], receivers=[])))
         self.assertFalse(node.validateTransaction(Transaction(senders=[(Wallet("first").address, 1)], receivers=[])))
         # self.assertFalse(node.validateTransaction(Transaction(senders=[], receivers=[(Wallet("second").address, 1)]))) not possible as it raises ValueError in the current implementation
-        self.assertFalse(node.validateTransaction(Transaction(senders=[(Wallet("first").address, 1), (Wallet("first").address, 1)], receivers=[(Wallet("second").address, 1)])))
-        self.assertTrue(node.validateTransaction(Transaction(senders=[(Wallet("first").address, 1)], receivers=[(Wallet("second").address, 1)])))
+        self.assertFalse(node.validateTransaction(
+            Transaction(senders=[(Wallet("first").address, 1), (Wallet("first").address, 1)],
+                        receivers=[(Wallet("second").address, 1)])))
+
+        self.assertTrue(node.validateTransaction(
+            Transaction(senders=[(Wallet("first").address, 1)], receivers=[(Wallet("second").address, 1)])))
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
