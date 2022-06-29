d567a1d646358ef020fa1abd2e45b94488ef2d32
Donneger Etienne <23462475+Krow10@users.noreply.github.com>
2022-06-29 15:14:58 -0400

    PoC communication entre les noeuds fonctionnel (partage nouveau bloc et vérification)

--- app/TCPClient.py
+++ app/TCPClient.py
@@ -4,17 +4,22 @@ from typing import Tuple
 
 class TCPClient(object):
     """docstring for TCPClient"""
-    def __init__(self):
+    def __init__(self, server_addr):
         super(TCPClient, self).__init__()
         self.peers = {} # Key : (HOST, PORT) / Value : socket reprensenting the peer connection
+        self.server_addr = server_addr
 
     def connect(self, peer: Tuple[str, int]) -> bool:
+        if (peer in self.peers): # Prevent connecting back to already connected peers
+            return False
+
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
             sock.connect(peer)
             self.peers[peer] = sock
+            sock.send(json.dumps({'connect': self.server_addr}).encode('utf-8')) # Sends server listening port for the remote peer to connect
         except Exception as e:
-            print("Error connect: ", e)
+            print("[ERROR] connect: ", e)
             return False # TODO : Handle connect exception
 
         return True
@@ -23,7 +28,7 @@ class TCPClient(object):
         try:
             self.peers[peer].close()
         except Exception as e:
-            print("Error disconnect: ", e)
+            print("[ERROR] close: ", e)
             return False # TODO : Handle close exception
 
         if clear:
@@ -35,18 +40,5 @@ class TCPClient(object):
             try:
                 sock.send(json.dumps(data).encode('utf-8'))
             except Exception as e:
-                print("Error send: ", e)
+                print("[ERROR] send: ", e)
                 pass # TODO : Handle send exception
-
-if __name__ == "__main__":
-    HOST, PORT = "localhost", 13337
-    data = {
-    "name": "hello, I am Tom.",
-    "age": 10,
-    "info": "sample is simple."
-    }
-
-    client = TCPClient()
-    client.connect((HOST, PORT))
-    client.broadcast(data)
-    client.disconnect((HOST, PORT))
--- app/TCPHandler.py
+++ app/TCPHandler.py
@@ -1,21 +1,45 @@
 import socketserver
 import json
 
+from Block import *
+from TransactionStore import *
+
 '''
     See https://docs.python.org/3/library/socketserver.html#request-handler-objects for reference
 '''
 class TCPHandler(socketserver.BaseRequestHandler):
     def handle(self):
-        fullnode = self.server
-        request_sock = self.request # Peer connection
+        self.fullnode = self.server
+        keep_alive = True
 
-        try:
-            data = request_sock.recv(1024)  # clip input at 1Kb
-            text = data.decode('utf-8')
+        while (keep_alive):
+            data = self.request.recv(1024)  # TODO : Fix in case payload gets bigger than 1Kb
+            text = data.decode("utf-8")
             json_payload = json.loads(text) # TODO : Handle exceptions
-            print(f"Received from {self.client_address} : {json_payload}")
-        except:
-            print("Error recv")
-            pass # TODO : Handle recv exception
+            print(f"[DEBUG] Received {len(data)} bytes from {self.client_address} :\n{json.dumps(json_payload, indent=4, sort_keys=True)}\n")
 
-        request_sock.close()
+            keep_alive = self.parseJSON(json_payload)
+
+        self.fullnode.client.disconnect(self.client_address) # Disconnects but do not remove the peer from the peers list
+        self.request.close()
+        print(f"[+] Closed connection with {self.client_address}")
+
+    def parseJSON(self, data: dict) -> bool:
+        if ("connect" in data):
+            server_address = tuple(data['connect'])
+            if (self.fullnode.client.connect(server_address)):
+                print(f"[*] Connected back to {server_address}...")
+            else:
+                print(f"[!] Already connected to {server_address} !")
+        elif ("newBlock" in data):
+            b = json.loads(data["newBlock"], object_hook=lambda d: Block(**d) if ("miner" in d) else TransactionStore(**d)) # TODO : Write proper JSON decoder for objects
+            print(b)
+            if (self.fullnode.validateNewBlock(b)):
+                print(f"[+] Validated block #{b.height} (hash: {b.getHash()}) !")
+                self.fullnode.blockchain.addBlock(b)
+            else:
+                print(f"[!] Block #{b.height} invalid: hash={b.getHash()}, lastBlock.height={self.fullnode.blockchain.lastBlock.height}")
+        elif ("end" in data):
+            return False
+
+        return True
