9be2c9c021fb049d6fe8ae35804c043d2a45288d
Alexandre Ondet <alexandre.ondet@telecomnancy.net>
2022-06-29 22:54:50 -0400

    add dummy transactions

--- app/FullNode.py
+++ app/FullNode.py
@@ -2,6 +2,7 @@ import time
 import socketserver
 import sys
 from typing import Tuple
+import json
 
 from Block import *
 from Blockchain import *
@@ -12,26 +13,39 @@ from Transaction import *
 from TransactionStore import *
 from Wallet import *
 
-
 '''
     See https://docs.python.org/3/library/socketserver.html#module-socketserver for reference. 
     Requests are handled by spawning a new instance of 'TCPHandler' in its own thread, calling its 'handle' function.
 '''
+
+
 class FullNode(socketserver.ThreadingTCPServer):
     """docstring for FullNode"""
-    daemon_threads = True # Stops server from blocking on abrupt shutdown
+    daemon_threads = True  # Stops server from blocking on abrupt shutdown
     allow_reuse_address = True
 
-    def __init__(self, consensusAlgorithm: bool, existing_wallet: Wallet, server_address: Tuple[str, int] = ('localhost', 13337), RequestHandlerClass: socketserver.BaseRequestHandler = TCPHandler):
-        socketserver.ThreadingTCPServer.__init__(self, server_address, RequestHandlerClass) # Initialize the TCP server for handling peer requests
+    def __init__(self, consensusAlgorithm: bool, existing_wallet: Wallet,
+                 server_address: Tuple[str, int] = ('localhost', 13337),
+                 RequestHandlerClass: socketserver.BaseRequestHandler = TCPHandler):
+        socketserver.ThreadingTCPServer.__init__(self, server_address,
+                                                 RequestHandlerClass)  # Initialize the TCP server for handling peer requests
         self.wallet = existing_wallet
-        self.consensusAlgorithm = ProofOfWork(1) if not consensusAlgorithm else None  # TODO : Change to ProofOfStake and set difficulty accordingly
-        self.transaction_pool = []
-        self.client = TCPClient(server_addr=server_address) # Create the TCPClient to interact with other peers
+        self.consensusAlgorithm = ProofOfWork(
+            1) if not consensusAlgorithm else None  # TODO : Change to ProofOfStake and set difficulty accordingly
+        self.client = TCPClient(server_addr=server_address)  # Create the TCPClient to interact with other peers
         self.blockchain = Blockchain()  # TODO : ask peers for blockchain state
+        self.transaction_pool = []
+        self.initTransactionPool()
 
     def __del__(self):
         self.server_close()
+
+    def initTransactionPool(self):
+        f = open('transaction_loop.json')
+        data = json.load(f)
+        for i in data['transactions']:
+            self.transaction_pool.append(Transaction(list(i['senders']), list(i['receivers'])))
+        f.close()
 
     def addToTransactionPool(self, t: Transaction):
         self.transaction_pool.append(t)
@@ -40,7 +54,8 @@ class FullNode(socketserver.ThreadingTCPServer):
         previous_block = self.blockchain.lastBlock
         return Block(
             timestamp=time.time(),
-            transactionStore=TransactionStore(self.transaction_pool),
+            transactionStore=TransactionStore(self.transaction_pool[previous_block.height % len(self.transaction_pool)]),
+            # each time, we pick the next transaction in the pool
             height=previous_block.height + 1,
             consensusAlgorithm=False,
             previousHash=previous_block.getHash(),
@@ -50,7 +65,8 @@ class FullNode(socketserver.ThreadingTCPServer):
     def computeReward(self) -> int:
         return 1  # TODO : Compute reward, maybe according to consensus algorithm or external rules ?
 
-    def mineNewBlock(self): # TODO : rework to be able to interrupt mining once a valid block has been received for the same height 
+    def mineNewBlock(
+            self):  # TODO : rework to be able to interrupt mining once a valid block has been received for the same height
         new_block = self.createNewBlock()
         self.consensusAlgorithm.mine(new_block)
         self.blockchain.addBlock(new_block)
@@ -63,7 +79,7 @@ class FullNode(socketserver.ThreadingTCPServer):
     def validateTransaction(self, t: Transaction) -> bool:
         if not (any(t.senders)
                 and any(t.receivers)
-                and len(t.senders) == len(set(t.senders))): # Check for duplicate inputs
+                and len(t.senders) == len(set(t.senders))):  # Check for duplicate inputs
             return False
 
         for (addr, amount) in t.senders:
@@ -86,10 +102,11 @@ class FullNode(socketserver.ThreadingTCPServer):
 
     def validateNewBlock(self, newBlock: Block) -> bool:
         if ((len(self.blockchain.blockChain) and newBlock.height <= self.blockchain.lastBlock.height)
-            or newBlock.previousHash != self.blockchain.lastBlock.getHash()
-            or newBlock.getHash()[0:self.consensusAlgorithm.blockDifficulty] != '0' * self.consensusAlgorithm.blockDifficulty
-            or newBlock.timestamp - time.time() > 3600  # Prevent block from being too much in the future (1h max)
-            or newBlock.reward != self.computeReward()):
+                or newBlock.previousHash != self.blockchain.lastBlock.getHash()
+                or newBlock.getHash()[
+                   0:self.consensusAlgorithm.blockDifficulty] != '0' * self.consensusAlgorithm.blockDifficulty
+                or newBlock.timestamp - time.time() > 3600  # Prevent block from being too much in the future (1h max)
+                or newBlock.reward != self.computeReward()):
             return False
 
         return all(
--- app/Transaction.py
+++ app/Transaction.py
@@ -1,5 +1,9 @@
+from json import dumps
+
+
 class Transaction:
     def __init__(self, senders, receivers):
+        """senders and receivers are a list of tuples with the addresses and the amounts"""
         total_in = 0
         for i in senders:
             total_in += i[1]
@@ -11,6 +15,9 @@ class Transaction:
         if total_out > total_in:
             raise ValueError("Sum of amount in must be >= Sum of amount out")
         self.receivers = receivers
+
+    def toJSON(self):
+        return dumps(self, default=lambda o: o.__dict__, sort_keys=True)
 
     def __repr__(self):
         return f"in:{self.senders}, out:{self.receivers}"
