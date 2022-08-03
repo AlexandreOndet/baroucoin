530b952c8ddd2db31aa2f44524df6fd7a66a5aca
Alexandre Ondet <alexandre.ondet@telecomnancy.net>
2022-08-02 23:06:09 -0400

    added half block difficulty

--- app/ProofOfWork.py
+++ app/ProofOfWork.py
@@ -1,4 +1,6 @@
 from app.ConsensusAlgorithm import *
+from math import modf
+
 
 class ProofOfWork(ConsensusAlgorithm, dict):
     def __init__(self, blockDifficulty):
@@ -6,11 +8,21 @@ class ProofOfWork(ConsensusAlgorithm, dict):
         self.blockDifficulty = blockDifficulty
 
     def mine(self, block):
+        frac, whole = modf(self.blockDifficulty)
+        whole = int(whole)
         self.alreadyFound = False
-        while not self.alreadyFound and block.getHash()[0:self.blockDifficulty] != '0' * self.blockDifficulty:
-            block.nonce = block.nonce + 1
+        if frac == 0:
+            while not self.alreadyFound and block.getHash()[0:self.blockDifficulty] != '0' * self.blockDifficulty:
+                block.nonce = block.nonce + 1
 
-        return not self.alreadyFound # True if node found the block and False if others found it
+            return not self.alreadyFound  # True if node found the block and False if others found it
+        elif frac == 0.5:
+            while not self.alreadyFound and (block.getHash()[0:whole + 1] != '0' * whole + '1' and block.getHash()[0:whole + 1] != '0' * (whole + 1)):
+                block.nonce = block.nonce + 1
+
+            return not self.alreadyFound  # True if node found the block and False if others found it
+        else:
+            raise ValueError("blockDIfficulty must be an integer or a float with a decimal part equals to 0.5")
 
     def stopMining(self):
         self.alreadyFound = True
